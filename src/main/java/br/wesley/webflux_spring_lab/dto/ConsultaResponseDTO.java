7e5193f7be65ffed1d70f1f6ea90598025202eef
wesleymrosa <wesleymrosa@gmail.com>
2025-05-09 21:34:07 -0300

    Adicionado DTO ConsultaResponseDTO com id, paciente, médico, dataHora e confirmação

---
+++ src/main/java/br/wesley/webflux_spring_lab/dto/ConsultaResponseDTO.java
@@ -0,0 +1,51 @@
+package br.wesley.webflux.dto;
+
+import java.time.LocalDateTime;
+
+public class ConsultaResponseDTO {
+    private Long id;
+    private String paciente;
+    private String medico;
+    private LocalDateTime dataHora;
+    private Boolean confirmada;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getMedico() {
+        return medico;
+    }
+
+    public void setMedico(String medico) {
+        this.medico = medico;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public Boolean getConfirmada() {
+        return confirmada;
+    }
+
+    public void setConfirmada(Boolean confirmada) {
+        this.confirmada = confirmada;
+    }
+}

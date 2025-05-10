c345c0a42b89acb57272a57402970679c6445940
wesleymrosa <wesleymrosa@gmail.com>
2025-05-09 21:11:59 -0300

    Projeto inicial do webflux-spring-lab configurado com Spring WebFlux, R2DBC e PostgreSQL

---
+++ src/main/java/br/wesley/webflux_spring_lab/WebfluxSpringLabApplication.java
@@ -0,0 +1,13 @@
+package br.wesley.webflux_spring_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxSpringLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxSpringLabApplication.class, args);
+	}
+
+}

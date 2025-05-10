84f24cffe918b95eda9e8b672656e1d2cf449cc6
wesleymrosa <wesleymrosa@gmail.com>
2025-05-09 21:30:11 -0300

    Adicionado repositório reativo para entidade Consulta com suporte a múltiplos filtros

---
+++ src/main/java/br/wesley/webflux_spring_lab/repository/ConsultaRepository.java
@@ -0,0 +1,25 @@
+package br.wesley.webflux.repository;
+
+import br.wesley.webflux.model.Consulta;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDateTime;
+
+public interface ConsultaRepository extends ReactiveCrudRepository<Consulta, Long> {
+
+    Flux<Consulta> findByPacienteContainingIgnoreCase(String paciente);
+
+    Flux<Consulta> findByMedicoContainingIgnoreCase(String medico);
+
+    Flux<Consulta> findByDataHoraBetween(LocalDateTime inicio, LocalDateTime fim);
+
+    Flux<Consulta> findByConfirmada(Boolean confirmada);
+
+    Flux<Consulta> findByPacienteAndMedico(String paciente, String medico);
+
+    Flux<Consulta> findByPacienteAndConfirmada(String paciente, Boolean confirmada);
+
+    Flux<Consulta> findByPacienteAndMedicoAndDataHora(String paciente, String medico, LocalDateTime dataHora);
+}
+

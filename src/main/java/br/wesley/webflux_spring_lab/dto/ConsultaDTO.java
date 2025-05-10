5d6a08e31e6da18f46739a20d7b5dd6d2a42e1af
wesleymrosa <wesleymrosa@gmail.com>
2025-05-09 21:32:01 -0300

    Adicionado objeto de transferência ConsultaDTO contendo paciente, médico e dataHora

---
+++ src/main/java/br/wesley/webflux_spring_lab/dto/ConsultaDTO.java
@@ -0,0 +1,33 @@
+package br.wesley.webflux.dto;
+
+import java.time.LocalDateTime;
+
+public class ConsultaDTO {
+    private String paciente;
+    private String medico;
+    private LocalDateTime dataHora;
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getMedico() {
+        return medico;
+    }
+
+    public void setMedico(String medico) {
+        this.medico = medico;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+}
